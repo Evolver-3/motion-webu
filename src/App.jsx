2f039d0ac609beaca21cc92d5c4afbac96c5cb3b
ASHU SINGH <Ashusingh2201@gmail.com>
2025-12-07 22:34:32 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Hero from "./component/herosection/Hero"
+
+
+const App = () => {
+  return (
+    <div>
+
+    <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import menu from './menu.svg'
+import car from './car.png'
+export const assets={
+  menu,car
+}
